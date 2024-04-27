2a31559b59d27951694511f456fc1e5666e13f3b
becquer <becquer123@becq.becq.com>
2024-04-26 21:04:34 -0500

    bug segments fixed

--- src/static/main.js
+++ src/static/main.js
@@ -3,6 +3,7 @@ const ctx = canvas.getContext("2d");
 
 function drawLine(degrees, radius) {
   ctx.beginPath();
+  ctx.lineWidth = 1;
   ctx.moveTo(0, 0);
   let angle = (degrees * Math.PI) / 180;
   ctx.lineTo(radius * Math.cos(angle), radius * Math.sin(angle));
@@ -16,26 +17,40 @@ function drawRoulette({
   rotationDegree = 0,
   segments = 2,
 }) {
+  //rotate the canvas to make the move of the roulette
   ctx.translate(x, y);
   ctx.rotate((rotationDegree * Math.PI) / 180);
+  //draw the circuference
   ctx.beginPath();
   ctx.arc(0, 0, radius, 0, 2 * Math.PI);
   ctx.stroke();
 
-  let segmentAngle = Math.floor(360 / segments);
+  //draw the strokes of each segment
+  const segmentAngle = Math.floor(360 / segments);
+  let angle = segmentAngle;
   drawLine(0, radius);
   new Array(segments - 1).fill(0).forEach(() => {
-    drawLine(segmentAngle, radius);
-    segmentAngle += segmentAngle;
+    drawLine(angle, radius);
+    angle += segmentAngle;
   });
 
   ctx.translate(-x, -y);
 }
 
-drawRoulette({
-  ctx,
-  position: { x: 100, y: 100 },
-  radius: 40,
-  rotationDegree: 0,
-  segments: 3,
-});
+let rotationDegree = 0;
+
+function main() {
+  canvas.width = canvas.width;
+  drawRoulette({
+    ctx,
+    position: { x: 200, y: 200 },
+    radius: 170,
+    rotationDegree,
+    segments: 1,
+  });
+
+  rotationDegree += 1;
+  requestAnimationFrame(main);
+}
+
+requestAnimationFrame(main);
