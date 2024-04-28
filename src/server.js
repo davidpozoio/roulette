654dfdeb68c8a119750ea013706e39a721c89881
becquer <becquer123@becq.becq.com>
2024-04-27 20:27:39 -0500

    page not found message added

--- src/server.js
+++ src/server.js
@@ -4,6 +4,12 @@ const app = express();
 
 app.use(express.static(path.resolve(__dirname, "static")));
 
+app.use("*", (req, res) => {
+  res.status(400).json({
+    message: "Page not found",
+  });
+});
+
 app.listen(8000, () => {
   console.log("the server has started in 8000");
 });
